dee144636e6287f25bac8105af88eb36b2e256c3
Marat-terabyte <marat.amzph21@gmail.com>
2024-04-16 21:02:10 +0300

    Added SocketFacade

---
+++ CloudStorage/CloudStorageLibrary/SocketFacade.cs
@@ -0,0 +1,58 @@
+﻿using System.Text;
+using System.Net.Sockets;
+
+namespace CloudStorageLibrary
+{
+    public class SocketFacade
+    {
+        public Socket Socket { get; set; }
+
+        public bool IsConnected { get => Socket.Connected; }
+
+        public SocketFacade(Socket socket)
+        {
+            Socket = socket;
+        }
+
+        public string Receive()
+        {
+            return Receive(Socket.ReceiveBufferSize);
+        }
+
+        public string Receive(int bufferSize)
+        {
+            if (!IsConnected)
+            {
+                Socket.Close();
+                throw new SocketException((int) SocketError.NotConnected, $"Socket is disconnected. Error code: {SocketError.NotConnected}");
+            }
+            
+            byte[] dataBytes = new byte[bufferSize];
+            
+            int numberOfBytes = Socket.Receive(dataBytes);
+            string data = Encoding.UTF8.GetString(dataBytes, 0, numberOfBytes);
+
+            return data;
+        }
+
+        public void Send(string data)
+        {
+            if (!IsConnected)
+            {
+                Socket.Close();
+                throw new SocketException((int)SocketError.NotConnected, $"Socket is disconnected. Error code: {SocketError.NotConnected}");
+            }
+
+            byte[] dataBytes = Encoding.UTF8.GetBytes(data);
+            Socket.Send(dataBytes);
+        }
+
+        public void CloseConnection()
+        {
+            if (IsConnected)
+                Socket.Shutdown(SocketShutdown.Both);
+            
+            Socket.Close();
+        }
+    }
+}
