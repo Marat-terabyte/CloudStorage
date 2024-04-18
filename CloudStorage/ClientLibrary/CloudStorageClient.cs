a704b8d945af4d249581b19c666f27157c97e867
Marat-terabyte <marat.amzph21@gmail.com>
2024-04-18 20:16:47 +0300

    Added CloudStorageServer and CloudStorageClient classes High level classes sending and receiving Request and Response classes

---
+++ CloudStorage/ClientLibrary/CloudStorageClient.cs
@@ -0,0 +1,49 @@
+﻿// MIT License
+// Copyright (c) 2024 Marat
+
+using System.Net.Sockets;
+using CloudStorageLibrary;
+using CloudStorageLibrary.Serializers;
+using CloudStorageLibrary.Serializers.Exceptions;
+
+namespace Client
+{
+    /// <summary>
+    /// Implements the logic of communication between server and client
+    /// </summary>
+    internal class CloudStorageClient
+    {
+        private SocketFacade _socket;
+
+        public int ReceiveBufferSize { get; set; } = 1024;
+
+        public CloudStorageClient(SocketFacade socket)
+        {
+            _socket = socket;
+        }
+
+        /// <summary> Receives a string response from the server and deserializes it </summary>
+        /// <exception cref="SocketException"/>
+        /// <exception cref="NotSupportedStatus"/>
+        /// <exception cref="NotValidByteLength"/>
+        public Response ReceiveResponse()
+        {
+            string serializedResponse = _socket.Receive(ReceiveBufferSize);
+
+            return ResponseSerializer.Deserialize(serializedResponse);
+        }
+
+        /// <summary> Serializes a request and sends it to the server </summary>
+        /// <param name="request"> Client request to serialize and send </param>
+        /// <exception cref="SocketException"/>
+        public void SendRequest(Request request)
+        {
+            string serializedRequest = RequestSerializer.Serialize(request);
+
+            _socket.Send(serializedRequest);
+        }
+
+        /// <summary> Disconnects from the server </summary>
+        public void CloseConnection() => _socket.CloseConnection();
+    }
+}
---
+++ CloudStorage/Server/CloudStorageServer.cs
@@ -0,0 +1,48 @@
+﻿// MIT License
+// Copyright (c) 2024 Marat
+
+using CloudStorageLibrary;
+using CloudStorageLibrary.Serializers;
+using CloudStorageLibrary.Serializers.Exceptions;
+using System.Net.Sockets;
+
+namespace Server
+{
+    /// <summary>
+    /// Implements the logic of communication between server and client
+    /// </summary>
+    internal class CloudStorageServer
+    {
+        private SocketFacade _socket;
+
+        public int ReceiveBufferSize { get; set; } = 2048;
+
+        public CloudStorageServer(Socket socket)
+        {
+            _socket = new SocketFacade(socket);
+        }
+
+        /// <summary> Receives a string request from the user and deserializes it </summary>
+        /// <exception cref="SocketException"/>
+        /// <exception cref="NotSupportedCommand"/>
+        public Request ReceiveRequest()
+        {
+            string serializedRequest = _socket.Receive();
+
+            return RequestSerializer.Deserialize(serializedRequest);
+        }
+
+        /// <summary> Serializes a response and sends it to the user </summary>
+        /// <param name="response"> Server response to serialize and send </param>
+        /// <exception cref="SocketException"/>
+        public void SendResponse(Response response)
+        {
+            string serializedResponse = ResponseSerializer.Serialize(response);
+
+            _socket.Send(serializedResponse);
+        }
+
+        /// <summary> Disconnects the user </summary>
+        public void CloseConnection() => _socket.CloseConnection();
+    }
+}
