56c5b07c37ea51d302c665a8d3deb421a7e10a0f
Marat-terabyte <marat.amzph21@gmail.com>
2024-04-20 07:44:29 +0300

    Added server configuration Added Config containing config.json values and ConfigReader that reads config.json

---
+++ CloudStorage/Server/Configuration/Config.cs
@@ -0,0 +1,13 @@
+﻿// MIT License
+// Copyright (c) 2024 Marat
+
+namespace Configuration
+{
+    /// <summary>
+    /// Contains configuration file values
+    /// </summary>
+    internal class Config
+    {
+        public string? Host { get; set; }
+    }
+}
---
+++ CloudStorage/Server/Configuration/ConfigReader.cs
@@ -0,0 +1,24 @@
+﻿// MIT License
+// Copyright (c) 2024 Marat
+
+using Configuration;
+using System.Text.Json;
+
+namespace Server.Configuration
+{
+    /// <summary>
+    /// Reads the configuration file
+    /// </summary>
+    internal class ConfigReader
+    {
+        private readonly string _configFile = "config.json";
+
+        /// <summary>
+        /// Reads json from the configuration file
+        /// </summary>
+        public Config? ReadConfig()
+        {
+            return JsonSerializer.Deserialize<Config>(File.ReadAllText(_configFile));
+        }
+    }
+}
