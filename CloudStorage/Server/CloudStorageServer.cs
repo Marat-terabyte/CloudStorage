318806d5523a00026c18d51c8c4074be44fb890a
Marat-terabyte <marat.amzph21@gmail.com>
2024-05-08 16:25:54 +0300

    Added DataTransfer

--- CloudStorage/Server/CloudStorageServer.cs
+++ CloudStorage/Server/CloudStorageServer.cs
@@ -5,21 +5,26 @@ using CloudStorageLibrary;
 using CloudStorageLibrary.Serializers;
 using CloudStorageLibrary.Serializers.Exceptions;
 using System.Net.Sockets;
+using System.Text;
 
 namespace Server
 {
     /// <summary>
     /// Implements the logic of communication between server and client
     /// </summary>
-    internal class CloudStorageServer
+    internal class CloudStorageServer : IDisposable
     {
-        private SocketFacade _socket;
-
+        private bool _disposed;
+        
+        /// <summary><see cref="SocketFacade"/> for sending responses and receiving requests</summary>
+        public SocketFacade MainSocket { get; set; }
+        public SocketFacade DataTransfer { get; set; }
         public int ReceiveBufferSize { get; set; } = 2048;
 
-        public CloudStorageServer(Socket socket)
+        public CloudStorageServer(Socket socket, Socket dataTransfer)
         {
-            _socket = new SocketFacade(socket);
+            MainSocket = new SocketFacade(socket);
+            DataTransfer = new SocketFacade(dataTransfer);
         }
 
         /// <summary> Receives a string request from the user and deserializes it </summary>
@@ -30,7 +35,7 @@ namespace Server
             Request? request = null;
             while (request == null)
             {
-                string data = _socket.Receive(1024);
+                string data = MainSocket.Receive(1024);
                 request = RequestSerializer.Deserialize(data);
             }
 
@@ -44,10 +49,59 @@ namespace Server
         {
             string serializedResponse = ResponseSerializer.Serialize(response);
 
-            _socket.Send(serializedResponse);
+            MainSocket.Send(serializedResponse);
+        }
+
+        /// <summary>
+        /// Serializes <paramref name="response"/>, encodes <paramref name="message"/> and sends them to the user
+        /// </summary>
+        public void SendResponse(Response response, string message)
+        {
+            byte[] buffer = Encoding.UTF8.GetBytes(message);
+            response.DataLenght = buffer.Length;
+
+            SendResponse(response);
+            DataTransfer.SendBytes(buffer);
+        }
+
+        /// <summary>
+        /// Serializes <paramref name="response"/>, sends it and <paramref name="message"/>
+        /// </summary>
+        public void SendResponse(Response response, byte[] message)
+        {
+            response.DataLenght = message.Length;
+
+            SendResponse(response);
+            DataTransfer.SendBytes(message);
         }
 
         /// <summary> Disconnects the user </summary>
-        public void CloseConnection() => _socket.CloseConnection();
+        public void CloseConnection()
+        {
+            MainSocket.CloseConnection();
+            DataTransfer.CloseConnection();
+        }
+
+        // Implementation of Dispose pattern
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!_disposed)
+            {
+                if (disposing)
+                {
+                    CloseConnection();
+                    MainSocket.Dispose();
+                    DataTransfer.Dispose();
+                }
+
+                _disposed = true;
+            }
+        }
     }
 }
