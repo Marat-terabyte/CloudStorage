03aa3ff056c6bf35f136b6932d7dc19ba14e7ae3
Marat-terabyte <marat.amzph21@gmail.com>
2024-05-08 16:26:21 +0300

    Added download command

---
+++ CloudStorage/Server/Commands/DownloadCommand.cs
@@ -0,0 +1,73 @@
+﻿using System.Text;
+using CloudStorageLibrary;
+using CloudStorageLibrary.Commands;
+
+namespace Server.Commands
+{
+    internal class DownloadCommand : RequestCommand
+    {
+        private string _basePath;
+        private FileTransfer _fileTransfer;
+
+        public DownloadCommand(CloudStorageServer server, string basePath) : base(server)
+        {
+            _basePath = basePath;
+            _fileTransfer = new FileTransfer(server.DataTransfer);
+        }
+
+        protected override void DoAction(Request request)
+        {
+            string file = _basePath + request.Args[0];
+
+            Response response = new Response(CommandStatus.Ok, new FileInfo(file).Length);
+            _server!.SendResponse(response);
+            _fileTransfer.SendFile(file);
+
+        }
+
+        protected override bool CanExecute(object parameter, out string? errorMessage)
+        {
+            errorMessage = null;
+
+            string[]? paths = parameter as string[];
+            if (paths == null)
+            {
+                errorMessage = "Not correct request";
+
+                return false;
+            }
+
+            string[] notExistFiles = CheckFiles(paths);
+            if (notExistFiles.Length != 0)
+            {
+                if (notExistFiles.Length == 1)
+                    errorMessage = $"{notExistFiles[0]} does not exist";
+                else
+                    errorMessage = string.Join(", ", notExistFiles) + " do not exist";
+
+                return false;
+            }
+
+            return true;
+        }
+
+        /// <summary>
+        /// Checks if files exist in the <paramref name="filenames"/> array
+        /// </summary>
+        /// <returns>Not exist files</returns>
+        private string[] CheckFiles(string[] filenames)
+        {
+            List<string> notExistFiles = new List<string>();
+
+            foreach (string filename in filenames)
+            {
+                if (!File.Exists(_basePath + filename))
+                {
+                    notExistFiles.Add(_basePath + filename);
+                }
+            }
+
+            return notExistFiles.ToArray();
+        }
+    }
+}
