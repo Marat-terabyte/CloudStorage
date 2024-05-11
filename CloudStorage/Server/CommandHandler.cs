1ac61c2e7dbd9b39597596be9bbae7279df18d6b
Marat-terabyte <marat.amzph21@gmail.com>
2024-05-11 10:58:20 +0300

    Integrated the upload, sign in, sign up commands

--- CloudStorage/Server/CommandHandler.cs
+++ CloudStorage/Server/CommandHandler.cs
@@ -4,19 +4,21 @@
 using Server.Commands;
 using CloudStorageLibrary;
 using CloudStorageLibrary.Commands;
+using Server.Database;
+using Server.Database.Models;
 
 namespace Server
 {
     internal class CommandHandler
     {
         public CloudStorageServer Server { get; set; }
-        public SocketFacade DataTransfer { get; set; }
         public string BasePath { get; set; }
+        public IRepository<User> Repository { get; set; }
 
-        public CommandHandler(CloudStorageServer server, SocketFacade dataTransfer, string basePath)
+        public CommandHandler(CloudStorageServer server, IRepository<User> repository, string basePath)
         {
             Server = server;
-            DataTransfer = dataTransfer;
+            Repository = repository;
             BasePath = basePath;
         }
 
@@ -30,6 +32,12 @@ namespace Server
                     return new DownloadCommand(Server, BasePath);
                 case Command.Remove:
                     return new RemoveCommand(Server, BasePath);
+                case Command.Upload:
+                    return new UploadCommand(Server, BasePath);
+                case Command.SignIn:
+                    return new SignInCommand(Server, Repository);
+                case Command.SignUp:
+                    return new SignUpCommand(Server, Repository);
                 default:
                     return null;
             }
