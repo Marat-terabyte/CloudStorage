468c7ba5dbe522df5242603675f8c1f5fc3aaec5
Marat-terabyte <marat.amzph21@gmail.com>
2024-05-11 10:48:37 +0300

    Added upload command

---
+++ CloudStorage/Server/Commands/UploadCommand.cs
@@ -0,0 +1,55 @@
+﻿// MIT License
+// Copyright (c) 2024 Marat
+
+using CloudStorageLibrary;
+using CloudStorageLibrary.Commands;
+
+namespace Server.Commands
+{
+    internal class UploadCommand : RequestCommand
+    {
+        private string _basePath;
+        private FileTransfer _fileTransfer;
+
+        public UploadCommand(CloudStorageServer server, string basePath) : base(server)
+        {
+            _basePath = basePath;
+            _fileTransfer = new FileTransfer(server.DataTransfer);
+        }
+
+        protected override bool CanExecute(Request request, out string? errorMessage)
+        {
+            if (request == null || request.Args.Length < 2)
+            {
+                errorMessage = "Not correct request";
+
+                return false;
+            }
+            errorMessage = null;
+            
+            // The first argument is filename
+            // The second argument is size of a file
+
+            bool isSize = long.TryParse(request.Args[1], out long size);
+            if (!isSize)
+            {
+                errorMessage = "File size not received";
+
+                return false;
+            }
+
+            return true;
+        }
+
+        protected override void DoAction(Request request)
+        {
+            string filename = request.Args[0];
+            long size = long.Parse(request.Args[1]);
+
+            Response response = new Response(CommandStatus.Ok);
+            _server.SendResponse(response);
+            
+            _fileTransfer.ReceiveFile(_basePath + filename, size);
+        }
+    }
+}
