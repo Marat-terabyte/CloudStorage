50e72e158e505553f6c55b824b0c040d65c2a5b8
Marat-terabyte <marat.amzph21@gmail.com>
2024-05-11 10:53:50 +0300

    Added sign up command

---
+++ CloudStorage/Server/Commands/SignUpCommand.cs
@@ -0,0 +1,73 @@
+﻿// MIT License
+// Copyright (c) 2024 Marat
+
+using CloudStorageLibrary;
+using CloudStorageLibrary.Commands;
+using Server.Database;
+using Server.Database.Models;
+
+namespace Server.Commands
+{
+    internal class SignUpCommand : RequestCommand
+    {
+        IRepository<User> _database;
+
+        public SignUpCommand(CloudStorageServer server, IRepository<User> repository) : base(server)
+        {
+            _database = repository;
+        }
+
+        protected override bool CanExecute(Request request, out string? errorMessage)
+        {
+            errorMessage = null;
+
+            if (request == null || request.Args.Length == 0)
+            {
+                errorMessage = "Not correct request";
+                
+                return false;
+            }
+
+            string username = request.Username;
+            if (username.Length < 4 || username.Contains(' '))
+            {
+                errorMessage = "Username must be more than 4 characters and must not contain spaces";
+                
+                return false;
+            }
+
+            User? user = _database.GetAll().Where(u => u.Username == username).FirstOrDefault();
+            if (user != null)
+            {
+                errorMessage = "A user with the same username already exists";
+
+                return false;
+            }
+
+            string password = request.Args[0];
+            if (password.Length < 8)
+            {
+                errorMessage = "Password must be more than 8 characters";
+
+                return false;
+            }
+
+            return true;
+        }
+
+        protected override void DoAction(Request request)
+        {
+            User user = new User();
+            user.Username = request.Username;
+            user.Password = request.Args[0];
+
+            _database.Insert(user);
+            _database.Save();
+
+            Directory.CreateDirectory(user.Username);
+
+            Response response = new Response(CommandStatus.Ok);
+            _server.SendResponse(response, "The account created");
+        }
+    }
+}
