05ae11b50692c96313ca22c6d2c47036e950a77e
Marat-terabyte <marat.amzph21@gmail.com>
2024-05-13 21:56:41 +0300

    Added socket builder

---
+++ CloudStorage/Server/SocketBuilder.cs
@@ -0,0 +1,20 @@
+﻿// MIT License
+// Copyright (c) 2024 Marat
+
+using System.Net;
+using System.Net.Sockets;
+
+namespace Server
+{
+    internal static class SocketBuilder
+    {
+        public static Socket Build(string ipAddress, int port)
+        {
+            Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            socket.Bind(new IPEndPoint(IPAddress.Parse(ipAddress), port));
+            socket.Listen();
+
+            return socket;
+        }
+    }
+}
