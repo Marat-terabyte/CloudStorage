0793cbb90156051d7840708c93b4b8258d72e021
Marat-terabyte <marat.amzph21@gmail.com>
2024-05-14 09:54:55 +0300

    Added base command class

---
+++ CloudStorage/ClientLibrary/Commands/ClientCommand.cs
@@ -0,0 +1,27 @@
+﻿// MIT License
+// Copyright (c) 2024 Marat
+
+using System.Net;
+using System.Net.Sockets;
+
+namespace ClientLibrary.Commands
+{
+    internal abstract class ClientCommand
+    {
+        public CloudStorageClient Client { get; private set; }
+
+        /// <summary>Connects to the server and initializes <see cref="Client"/></summary>
+        /// <exception cref="SocketException"></exception>
+        public ClientCommand()
+        {
+            Socket mainSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            Socket dataTransfer = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            IPAddress address = IPAddress.Parse(ServerInfo.Host);
+            mainSocket.Connect(new IPEndPoint(address, ServerInfo.MainPort));
+            dataTransfer.Connect(new IPEndPoint(address, ServerInfo.DataTransferPort));
+
+            Client = new CloudStorageClient(mainSocket, dataTransfer);
+        }
+    }
+}
