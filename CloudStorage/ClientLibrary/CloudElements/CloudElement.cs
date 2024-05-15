a502ec83c4d104fe8a189e54df2a441d2d3b6735
Marat-terabyte <marat.amzph21@gmail.com>
2024-05-15 19:11:56 +0300

    Added cloud element classes

---
+++ CloudStorage/ClientLibrary/CloudElements/CloudElement.cs
@@ -0,0 +1,20 @@
+﻿// MIT License
+// Copyright (c) 2024 Marat
+
+namespace ClientLibrary.CloudElements
+{
+    public class CloudElement
+    {
+        public string Name
+        {
+            get => new FileInfo(Path).Name;
+        }
+        
+        public string Path { get; set; }
+
+        public CloudElement(string path)
+        {
+            Path = path;
+        }
+    }
+}
