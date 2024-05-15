80d2611485a95b2018c04b3c55c5fe7046e6a006
Marat-terabyte <marat.amzph21@gmail.com>
2024-05-15 19:03:59 +0300

    Added 'RequestBuilder' and 'UserInfo'

---
+++ CloudStorage/ClientLibrary/RequestBuilder.cs
@@ -0,0 +1,22 @@
+﻿// MIT License
+// Copyright (c) 2024 Marat
+
+using CloudStorageLibrary;
+using CloudStorageLibrary.Commands;
+
+namespace ClientLibrary
+{
+    internal static class RequestBuilder
+    {
+        public static Request Build(Command command, string[] args)
+        {
+            Request request = new Request();
+            request.SessionId = UserInfo.SessionId;
+            request.Username = UserInfo.Username;
+            request.Command = command;
+            request.Args = args;
+
+            return request;
+        }
+    }
+}
---
+++ CloudStorage/ClientLibrary/UserInfo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientLibrary
+{
+    internal static class UserInfo
+    {
+        public static long? SessionId { get; set; }
+        public static string? Username { get; set; }
+    }
+}
