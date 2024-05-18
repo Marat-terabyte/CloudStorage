785f8c6154f71f0e15d29b835162635e85ee31d0
Marat-terabyte <marat.amzph21@gmail.com>
2024-05-18 23:01:53 +0300

    Added sign in command

---
+++ CloudStorage/ClientLibrary/Commands/SignInCommand.cs
@@ -0,0 +1,49 @@
+﻿// MIT License
+// Copyright (c) 2024 Marat
+
+using CloudStorageLibrary;
+using CloudStorageLibrary.Commands;
+
+namespace ClientLibrary.Commands
+{
+    public class SignInCommand : ClientCommand
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+
+        public SignInCommand(string username, string password)
+        {
+            Username = username;
+            Password = password;
+        }
+
+        public bool Execute(out string? message)
+        {
+            Request request = RequestBuilder.Build(Command.SignIn, Username, [Password]);
+            Client.SendRequest(request);
+
+            Response response = Client.ReceiveResponse(out string? data);
+            if (response.Status == CommandStatus.Ok)
+            {
+                if (!long.TryParse(data, out long id))
+                {
+                    message = data;
+
+                    return false;
+                }
+
+                message = null;
+                UserInfo.Username = Username;
+                UserInfo.SessionId = id;
+
+                return true;
+            }
+            else
+            {
+                message = data;
+
+                return false;
+            }
+        }
+    }
+}
