33d62f57b838092a240ded3c249c14629cf18bdc
Marat-terabyte <marat.amzph21@gmail.com>
2024-05-18 23:01:37 +0300

    Added download command

---
+++ CloudStorage/ClientLibrary/Commands/DownloadCommand.cs
@@ -0,0 +1,49 @@
+﻿// MIT License
+// Copyright (c) 2024 Marat
+
+using ClientLibrary.CloudElements;
+using CloudStorageLibrary;
+using CloudStorageLibrary.Commands;
+
+namespace ClientLibrary.Commands
+{
+    public class DownloadCommand : ClientCommand
+    {
+        private FileTransfer _fileTransfer;
+
+        public CloudElement CloudElement { get; set; }
+        public string ToDir { get; set; } = "";
+
+        public DownloadCommand(CloudElement elementToDownload)
+        {
+            _fileTransfer = new FileTransfer(Client.DataSocket);
+            CloudElement = elementToDownload;
+        }
+
+        public DownloadCommand(CloudElement elementToDownload, string toDir)
+        {
+            _fileTransfer = new FileTransfer(Client.DataSocket);
+            CloudElement = elementToDownload;
+            ToDir = toDir;
+        }
+
+        public bool Execute(out string? message)
+        {
+            Request request = RequestBuilder.Build(Command.Download, [CloudElement.Path]);
+            Client.SendRequest(request);
+            
+            Response response = Client.ReceiveResponse();
+            if (response.Status == CommandStatus.NotOk)
+            {
+                message = Client.DataSocket.Receive((int)response.DataLenght);
+
+                return false;
+            }
+
+            _fileTransfer.ReceiveFile(Path.Combine(ToDir, CloudElement.Name), response.DataLenght);
+            message = $"{CloudElement.Name} downloaded successfully";
+
+            return true;
+        }
+    }
+}
