869bef345f1649a81a9f831d3ed83d2995c1a16f
Marat-terabyte <marat.amzph21@gmail.com>
2024-05-18 23:02:38 +0300

    Added sign up command

---
+++ CloudStorage/ClientLibrary/Commands/SignUpCommand.cs
@@ -0,0 +1,34 @@
+﻿// MIT License
+// Copyright (c) 2024 Marat
+
+using CloudStorageLibrary;
+using CloudStorageLibrary.Commands;
+
+namespace ClientLibrary.Commands
+{
+    public class SignUpCommand : ClientCommand
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+
+        public SignUpCommand(string username, string password)
+        {
+            Username = username;
+            Password = password;
+        }
+
+        public bool Execute(out string? message)
+        {
+            Request request = RequestBuilder.Build(Command.SignUp, Username, [Password]);
+            Client.SendRequest(request);
+
+            Response response = Client.ReceiveResponse(out string? data);
+            message = data;
+
+            if (response.Status == CommandStatus.Ok)
+                return true;
+            else
+                return false;
+        }
+    }
+}
