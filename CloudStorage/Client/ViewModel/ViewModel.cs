e90dbd2d4d18a1978e6eb2d25787a2a010113cdd
Marat-terabyte <marat.amzph21@gmail.com>
2024-05-28 20:29:38 +0300

    Added base viewmodel class

---
+++ CloudStorage/Client/ViewModel/ViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace Client.ViewModel
+{
+    public class ViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
