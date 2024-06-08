9e42cebe73e510309b000bfd44c73ccf8918c7ea
Marat-terabyte <marat.amzph21@gmail.com>
2024-06-08 19:25:17 +0300

    Added classes that extend CloudElement class for WPF

---
+++ CloudStorage/Client/Model/ExtendedCloudElement.cs
@@ -0,0 +1,49 @@
+﻿// MIT License
+// Copyright (c) 2024 Marat
+
+using System.ComponentModel;
+using System.IO;
+using System.Text;
+using System.Windows;
+using System.Windows.Input;
+using ClientLibrary.CloudElements;
+
+namespace Client.Model
+{
+    public abstract class ExtendedCloudElement : CloudElement
+    {
+        public string ImageSource { get; set; }
+
+        public ExtendedCloudElement(string path) : base(path)
+        {
+            ImageSource = "/Client;component/" + GetImageSource();
+        }
+
+        public abstract bool Download(out string? message);
+
+        protected virtual string GetImageSource()
+        {
+            switch (new FileInfo(Path).Extension)
+            {
+                case "":
+                    return "Assets/folder.png";
+                case ".png":
+                    return "Assets/png-file.png";
+                case ".pdf":
+                    return "Assets/pdf-file.png";
+                case ".exe":
+                    return "Assets/exe-file.png";
+                case ".doc":
+                    return "Assets/doc-file.png";
+                case ".docx":
+                    return "Assets/docx-file.png";
+                case ".txt":
+                    return "Assets/txt-file.png";
+                case ".html":
+                    return "Assets/html-file.png";
+                default:
+                    return "Assets/file.png";
+            }
+        }
+    }
+}
---
+++ CloudStorage/Client/Model/ExtendedCloudFile.cs
@@ -0,0 +1,24 @@
+﻿// MIT License
+// Copyright (c) 2024 Marat
+
+using ClientLibrary.Commands;
+using System.IO;
+using System.Windows;
+
+namespace Client.Model
+{
+    public class ExtendedCloudFile : ExtendedCloudElement
+    {
+        public ExtendedCloudFile(string path) : base(path)
+        {
+        }
+
+        public override bool Download(out string? message)
+        {
+            string toDir = new FileInfo(Path)?.Directory?.Name ?? "";
+            DownloadCommand command = new DownloadCommand(this, toDir);
+            
+            return command.Execute(out message);
+        }
+    }
+}
